6bb673d34c6fb66c8c6fb37cbad0c19da9fa8270
Sreeja Mynampati <smynampati@griddynamics.com>
2026-07-07 13:06:38 +0530

    Task 2: Configure Eureka Service Discovery

---
+++ discovery-server/src/test/java/com/sreeja/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreeja.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
