49af265db1b42e4a395f1c8d260e720393d4e2c0
Sreeja Mynampati <smynampati@griddynamics.com>
2026-07-09 14:43:55 +0530

    Task 5: Implement Web Security

---
+++ course-service/src/main/java/com/sreeja/courseservice/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.sreeja.courseservice;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/courses").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic();
+
+        return http.build();
+    }
+}
