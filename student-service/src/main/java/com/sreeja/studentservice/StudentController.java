914145ccbbf0ce4f0886f20156613826608f286b
Sreeja Mynampati <smynampati@griddynamics.com>
2026-07-10 11:13:46 +0530

    Add Resilience4j circuit breaker and retry support

--- student-service/src/main/java/com/sreeja/studentservice/StudentController.java
+++ student-service/src/main/java/com/sreeja/studentservice/StudentController.java
@@ -1,26 +1,34 @@
 package com.sreeja.studentservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Value;
+
 @RestController
 public class StudentController {
 
     @Autowired
     private RestTemplate restTemplate;
+
     @Value("${course.service.url}")
     private String courseServiceUrl;
 
     @GetMapping("/students")
+    @CircuitBreaker(name = "courseService", fallbackMethod = "fallbackCourses")
+    @Retry(name = "courseService")
     public String getStudents() {
 
-        String courses = restTemplate.getForObject(
+        return restTemplate.getForObject(
                 "http://COURSE-SERVICE/courses",
                 String.class
         );
+    }
 
-        return courses;
+    public String fallbackCourses(Exception ex) {
+        return "Course Service is currently unavailable. Please try again later.";
     }
 }
